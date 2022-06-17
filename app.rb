38408fb32af32fbf5cbc61b73e21b54fb237f8f9
Yuki Kuwashima <yukiny0811@gmail.com>
2022-06-18 00:10:48 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -7,28 +7,7 @@ not_found do
   redirect "/"
 end
 
-set :server, 'thin'
-set :sockets, []
-
 get '/' do
   erb :index
-end
-
-get '/websocket' do
-  if request.websocket?
-    request.websocket do |ws|
-      ws.onopen do
-        settings.sockets << ws
-      end
-      ws.onmessage do |msg|
-        settings.sockets.each do |s|
-          s.send(msg)
-        end
-      end
-      ws.onclose do
-        settings.sockets.delete(ws)
-      end
-    end
-  end
 end
 
